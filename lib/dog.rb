1178e973017dbd4e8b19ccd743ebda8bb0bca9c0
cindych419@gmail.com <cindych419@gmail.com>
2018-04-26 00:22:08 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,21 @@
 require 'pry'
 class Dog
-  def teaching(name)
+
+  def initialize(name)
     @name = name
   end
+
   def name
-    #binding.pry
     @name
   end
+
   def name=(new_name)
     @name = new_name
   end
-    def indentify(breed)
-      @bread = breed
-    end
+
+  def indentify(breed)
+    @bread = breed
+  end
 
   def breed
     @breed
